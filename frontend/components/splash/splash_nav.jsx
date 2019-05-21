991bcd3270baae15ae9af70787d79533d4d45453
Bryce Morgan <bamorgan13@gmail.com>
2019-05-21 14:02:43 -0400

    Add name to splash logo. Add spacing to session form

--- frontend/components/splash/splash_nav.jsx
+++ frontend/components/splash/splash_nav.jsx
@@ -5,7 +5,9 @@ const SplashNav = () => {
 	return (
 		<header className="splash-header">
 			<nav className="splash-header-nav">
-				<Link to="/" className="splash-nav-logo" />
+				<Link to="/" className="splash-nav-logo">
+					cha-aux
+				</Link>
 				<Link to="/login" className="splash-nav-login">
 					Login
 				</Link>
