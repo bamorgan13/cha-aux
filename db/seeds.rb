f50e4a4c27f3a2009ebdb6ffc268bfdad13ae23f
Bryce Morgan <bamorgan13@gmail.com>
2019-05-24 16:02:13 -0400

    Fix previous references to user to be specific in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,19 +15,19 @@ Server.destroy_all
 bryce = User.create({username: 'Bryce', email: 'bryce@gmail.com', password: 'notpassword'})
 bryce.icon_image.attach(io: open('https://s3.amazonaws.com/cha-aux-seeds/bryce_headshot.jpg'), filename:'bryce_icon.jpg')
 gaben = User.create({username: 'GabeN', email: 'GabeN@gmail.com', password: 'password'})
-gaben.icon_image.attach(io: open(Faker::Avatar.image(nil,"50x50")), filename:"#{user.username}_icon.jpg")
+gaben.icon_image.attach(io: open(Faker::Avatar.image(nil,"50x50")), filename:"#{gaben.username}_icon.jpg")
 zegyr = User.create({username: 'Zegyr', email: 'ryon@gmail.com', password: 'password'})
-zegyr.icon_image.attach(io: open(Faker::Avatar.image(nil,"50x50")), filename:"#{user.username}_icon.jpg")
+zegyr.icon_image.attach(io: open(Faker::Avatar.image(nil,"50x50")), filename:"#{zegy.username}_icon.jpg")
 chaos = User.create({username: 'Chaos', email: 'chaos@gmail.com', password: 'password'})
-chaos.icon_image.attach(io: open(Faker::Avatar.image(nil,"50x50")), filename:"#{user.username}_icon.jpg")
+chaos.icon_image.attach(io: open(Faker::Avatar.image(nil,"50x50")), filename:"#{chaos.username}_icon.jpg")
 durango = User.create({username: 'Durango', email: 'durango@gmail.com', password: 'password'})
-durango.icon_image.attach(io: open(Faker::Avatar.image(nil,"50x50")), filename:"#{user.username}_icon.jpg")
+durango.icon_image.attach(io: open(Faker::Avatar.image(nil,"50x50")), filename:"#{durango.username}_icon.jpg")
 jason = User.create({username: 'Jason', email: 'citronator@gmail.com', password: 'password'})
-jason.icon_image.attach(io: open(Faker::Avatar.image(nil,"50x50")), filename:"#{user.username}_icon.jpg")
+jason.icon_image.attach(io: open(Faker::Avatar.image(nil,"50x50")), filename:"#{jason.username}_icon.jpg")
 justin = User.create({username: 'Justin', email: 'kanman@twitch.tv', password: 'password'})
-justin.icon_image.attach(io: open(Faker::Avatar.image(nil,"50x50")), filename:"#{user.username}_icon.jpg")
+justin.icon_image.attach(io: open(Faker::Avatar.image(nil,"50x50")), filename:"#{justin.username}_icon.jpg")
 emmett = User.create({username: 'Emmett', email: 'shearbear@twitch.tv', password: 'password'})
-emmett.icon_image.attach(io: open(Faker::Avatar.image(nil,"50x50")), filename:"#{user.username}_icon.jpg")
+emmett.icon_image.attach(io: open(Faker::Avatar.image(nil,"50x50")), filename:"#{emmett.username}_icon.jpg")
 
 User.all.each do |user|
   user.owned_servers.create({name: 'Home', private: true})
@@ -44,7 +44,9 @@ everyday.icon_image.attach(io: open('https://s3.amazonaws.com/cha-aux-seeds/ever
 got = Server.create({name: 'Game of Thrones', owner_id: zegyr.id})
 got.icon_image.attach(io: open('https://s3.amazonaws.com/cha-aux-seeds/got_icon.jpg'), filename:'got_icon.jpg')
 
-User.all.each { |user| aa.memberships.create({user_id: user.id})}
-User.all.each { |user| mtga.memberships.create({user_id: user.id})}
+User.all.each do |user|
+  aa.memberships.create({user_id: user.id})
+  mtga.memberships.create({user_id: user.id})
+end
 [bryce, gaben, zegyr, chaos].each { |user| everyday.memberships.create({user_id: user.id})}
 [bryce, zegyr, durango, jason].each { |user| got.memberships.create({user_id: user.id})}
