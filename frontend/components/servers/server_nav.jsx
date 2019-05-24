7a2b62d49c6eeb8bc1043db407a78da80e83e65b
Bryce Morgan <bamorgan13@gmail.com>
2019-05-23 23:40:59 -0400

    Add server selector indicator to server nav

--- frontend/components/servers/server_nav.jsx
+++ frontend/components/servers/server_nav.jsx
@@ -14,22 +14,46 @@ class ServerNav extends React.Component {
 	}
 
 	render() {
+		const currentServerId = this.props.location.pathname.split('/')[2];
 		const serverLis = Object.keys(this.props.servers).map(serverId => {
 			const server = this.props.servers[serverId];
-			return <ServerNavItem key={serverId} server={server} />;
+			const activeServer = currentServerId === serverId ? 'active' : '';
+			// console.log(activeServer);
+			// debugger;
+			return <ServerNavItem key={serverId} server={server} activeServer={activeServer} />;
 		});
 		serverLis.splice(1, 0, <li key={0} className="server-nav-separator" />);
 		return (
 			<div className="server-container">
 				<nav className="server-nav">
-					<ul className="server-nav-list">{serverLis}</ul>
-					<Link to="/server-discovery" className="server-nav-create">
-						{/*PLACEHOLDER FOR MODAL BUTTON*/}
-						server creation modal
-					</Link>
-					<Link to="/server-discovery" className="server-nav-discovery">
-						Server Discovery
-					</Link>
+					<ul className="server-nav-list">
+						{serverLis}
+						<li className="server-nav-li">
+							<Link to="/server-discovery" className="server-nav-create">
+								{/*PLACEHOLDER FOR MODAL BUTTON*/}
+								server creation modal
+							</Link>
+							<div className="server-name-label-container">
+								<div className="server-nav-arrow-left" />
+								<span className="server-nav-li-link-name">Add a Server</span>
+							</div>
+							<div className="server-nav-selector-container">
+								<div className={`server-nav-selector-indicator`} />
+							</div>
+						</li>
+						<li className="server-nav-li">
+							<Link to="/server-discovery" className="server-nav-discovery">
+								Server Discovery
+							</Link>
+							<div className="server-name-label-container">
+								<div className="server-nav-arrow-left" />
+								<span className="server-nav-li-link-name">Server Discovery</span>
+							</div>
+							<div className="server-nav-selector-container">
+								<div className={`server-nav-selector-indicator`} />
+							</div>
+						</li>
+					</ul>
 				</nav>
 				<TempLogout />
 			</div>
