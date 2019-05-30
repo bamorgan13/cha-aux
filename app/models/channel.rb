521d6375efaf392a7fa9019f5b87033b912f3f0f
Bryce Morgan <bamorgan13@gmail.com>
2019-05-30 16:35:57 -0400

    Create channel model. Add channel association to server model and channelIds to _server json partial

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,21 +1,20 @@
 # == Schema Information
 #
-# Table name: servers
+# Table name: channels
 #
 #  id         :bigint           not null, primary key
 #  name       :string           not null
-#  owner_id   :integer          not null
+#  server_id  :integer
 #  private    :boolean          default(FALSE), not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 
-class Server < ApplicationRecord
+class Channel < ApplicationRecord
   validates :name, presence: true
 
-  belongs_to :owner,
-    foreign_key: :owner_id,
-    class_name: :User
+  belongs_to :server,
+    optional: true
 
   has_many :memberships,
     as: :joinable
@@ -23,9 +22,4 @@ class Server < ApplicationRecord
   has_many :members,
     through: :memberships,
     source: :user
-
-  has_many :channels,
-    dependent: :destroy
-
-  has_one_attached :icon_image
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_23_171755) do
+ActiveRecord::Schema.define(version: 2019_05_30_155904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 2019_05_23_171755) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "channels", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "server_id"
+    t.boolean "private", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["server_id", "name"], name: "index_channels_on_server_id_and_name", unique: true
   end
 
   create_table "memberships", force: :cascade do |t|
